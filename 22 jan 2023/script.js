70ad05c3bb2321a336501b9aa03fea463ec11d2e
Chetandasa <119198219+Chetandasa@users.noreply.github.com>
2023-02-14 23:32:41 +0530

    Java script

---
+++ 22 jan 2023/script.js
@@ -0,0 +1,117 @@
+// 1
+let startingNumber = 100;
+startingNumber = startingNumber + 1;
+console.log(startingNumber);
+
+let startingNumberTwo = 100;
+startingNumberTwo++;
+console.log(startingNumberTwo);
+
+// 2
+let startingNumberThree = 100;
+startingNumberThree--;
+console.log(startingNumberThree);
+
+let startingNumberFour = 100;
+startingNumberFour = startingNumberFour - 1;
+console.log(startingNumberFour);
+
+// 3
+let startingNumberFive = 100;
+startingNumberFive = startingNumberFive + 5;
+console.log(startingNumberFive);
+
+let startingNumbersix = 100;
+startingNumbersix+=5;
+console.log(startingNumbersix);
+
+// 4
+let startingNumberseven = 100;
+startingNumberseven = startingNumberseven - 5;
+console.log(startingNumberseven);
+
+let startingNumbereight = 100;
+startingNumbereight-=5;
+console.log(startingNumbereight);
+
+// start
+
+let numberOne = 10;
+let numberTwo = 20;
+let numberThree = 30;
+
+let result1 = numberOne + numberTwo * numberThree;
+console.log(result1);
+
+let result2 = (numberOne + numberTwo) * numberThree;
+console.log(result2);
+
+// start
+let numberWithBatman = 10;
+let numberWithSuperman = 5;
+let numberWithTombRaider = 15;
+
+// edi pan sudu chase
+// personValue = numberWithBatman;
+
+// if(personValue == 10)
+//   {
+//     console.log("going straight");
+//   }
+// if (personValue < 10)
+//   {
+//     console.log("going up");
+//   }
+// if (personValue > 10)
+//   {
+//     console.log("going down");
+//   }
+
+
+// edi pan sudu chase
+  personValue = numberWithSuperman;
+
+  if(personValue == 10)
+    {
+      console.log("going straight");
+    }
+    else{
+      console.log("unable to go strating, boolean is false")
+    }
+  if (personValue < 10)
+    {
+      console.log("going up");
+    }
+    else{
+      console.log("unable to go up, boolean is false")
+    }
+  if (personValue > 10)
+    {
+      console.log("going down");
+    }
+    else{
+      console.log("unable to go down, boolean is false")
+    }
+// END
+
+
+let timeOfEvening = 8.30;
+
+if(timeOfEvening >9.0){
+console.log("OH MY GOD.... DOING EXTRA TEACHING. STOP CLASS IMMEDIATELY");
+} 
+else if(timeOfEvening == 9.0){
+  console.log("perfect end time. stop class");
+} 
+else if (timeOfEvening >=8.0 && timeOfEvening <= 9.0){
+  console.log("think about giving break to students");
+} 
+else if (timeOfEvening < 7.0 || timeOfEvening < 6.0){
+  console.log("class not yet started");
+}
+else if (timeOfEvening!= 4.0){
+  console.log("helllooooooooo")
+}
+else {
+  console.log ("nothing to warry . carry on with class");
+}
---
+++ forloop/js1.js
@@ -0,0 +1,43 @@
+let startingPoint = 1;
+let endingPoint = 11;
+
+for (var i = startingPoint;  //part one
+  i<endingPoint;   //part three
+  i++   //part Two
+  )
+  {
+    console.log(i);
+  }
+
+
+
+
+
+  let startingPoint2 = 2;
+  let endingPoint2 = 22;
+
+  let counter = 0;
+  for (var i = startingPoint2;  //part one
+    i<endingPoint2;   //part three
+    i+=2   //part Two
+    )
+    {
+      var output = i + ".number is : " + i;
+      console.log(output);
+      counter++;
+    }
+
+
+    let startingPoint3 = 2;
+    let endingPoint3 = 22;
+    let counter3 = 101;
+
+    for (var i = startingPoint3;  //part one
+      i<endingPoint3;   //part three
+      i++   //part Two
+      )
+      {
+        if(i % 5 == 0) {
+          var output = counter3 + ". number is : " + i + "waring .."
+        }
+      }
